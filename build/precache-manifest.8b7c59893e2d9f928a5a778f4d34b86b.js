d98b3ff348457477c1bae9a3c735492756d502f9
Emre Can Kızılateş <kizilatesemrecan@gmail.com>
2023-06-05 17:18:17 +0300

    build

---
+++ build/precache-manifest.8b7c59893e2d9f928a5a778f4d34b86b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af93ef15c42b2e2efbd",
+    "url": "/static/css/main.d8479584.chunk.css"
+  },
+  {
+    "revision": "0af93ef15c42b2e2efbd",
+    "url": "/static/js/main.4476df99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "201491bc47b95b64a60d",
+    "url": "/static/css/2.562824ce.chunk.css"
+  },
+  {
+    "revision": "201491bc47b95b64a60d",
+    "url": "/static/js/2.46a3526b.chunk.js"
+  },
+  {
+    "revision": "77ad37c36df3f24dff149014562fce4a",
+    "url": "/static/media/adv-form-banner.77ad37c3.png"
+  },
+  {
+    "revision": "ee9afbeacbbca7e236ac4bfa0fa39a48",
+    "url": "/index.html"
+  }
+];
